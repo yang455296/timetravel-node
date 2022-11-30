ebae3b2656b719b2b4ffbcac2ea0d8f91b38487e
張華容 <chloe012125@gmail.com>
2022-11-30 18:03:39 +0800

    api更改

--- modules/db_connect2.js
+++ modules/db_connect2.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: process.env.DB_HOST || '192.168.35.119',
     user: process.env.DB_USER || 'project',
-    //port:'3307',
+    port:'3306',
     password: process.env.DB_PASS || '123',
     database: process.env.DB_NAME || 'project',
     waitForConnections: true,
