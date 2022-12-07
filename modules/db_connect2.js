a9df2f984e88703ee80326ef86966bffad4e97b1
Eliot Du <108873919+EliotDu@users.noreply.github.com>
2022-12-07 11:46:06 +0800

    merge

--- modules/db_connect2.js
+++ modules/db_connect2.js
@@ -2,22 +2,22 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'ProjectAtHome1202',
-    port:'3306',
-    waitForConnections: true,
-    connectionLimit: 10,  // 最大連線數
-    queueLimit: 0 //不限制排隊數量
-    // host: process.env.DB_HOST || '192.168.35.119',
-    // user: process.env.DB_USER || 'project',
-    // password: process.env.DB_PASS || '123',
-    // database: process.env.DB_NAME || 'project',
-    // // port:'3306',
+    // host: process.env.DB_HOST || 'localhost',
+    // user: process.env.DB_USER || 'root',
+    // password: process.env.DB_PASS || '',
+    // database: process.env.DB_NAME || 'ProjectAtHome1202',
+    // port:'3306',
     // waitForConnections: true,
     // connectionLimit: 10,  // 最大連線數
-    // queueLimit: 0, //不限制排隊數量
+    // queueLimit: 0 //不限制排隊數量
+    host: process.env.DB_HOST || '192.168.35.119',
+    user: process.env.DB_USER || 'project',
+    password: process.env.DB_PASS || '123',
+    database: process.env.DB_NAME || 'project',
+    // port:'3306',
+    waitForConnections: true,
+    connectionLimit: 10,  // 最大連線數
+    queueLimit: 0, //不限制排隊數量
    
 });
 module.exports = pool.promise();  // 滙出promise pool
